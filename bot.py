ff5da042720e46fce5252e341dd6fe7f1ea0f536
HOLY-WTER <monmonten@gmail.com>
2024-03-04 15:55:26 +0700

    add pan feature

--- bot.py
+++ bot.py
@@ -73,8 +73,31 @@ async def promt(ctx, *args):
     for chunk in response:
         await ctx.send(chunk.text)
 
+# @bot.command()
+# async def status(ctx, num: int):
+#     # await ctx.send(f"Here's an image:", file=discord.File('https://http.dog/[num].jpg', ))
+#     image_url = f"https://http.dog/{num}.jpg"
 
-
+#     await ctx.send(image_url)
+@bot.command()
+async def status(ctx, num: int):
+    
+    numbers = [100,101,102,103,200,201,202,203,
+               204,205,206,207,208,218,226,300,
+               301,302,303,304,305,306,307,308,
+               400,401,402,403,404,405,406,407,
+               408,409,410,411,412,413,414,415,
+               416,417,418,419,420,421,422,423,
+               424,425,426,428,429,430,431,440,
+               444,449,450,451,460,463,464,494,
+               495,496,497,498,499,500,501,502,
+               503,504,505,506,507,508,509,510,
+               511,520,521,522,523,524,525,526,
+               527,529,530,561,598,599,999]
+    if num in numbers:
+        image_url = f"https://http.dog/{num}.jpg"
+        # await ctx.send(f"Here's an image:", image_url)
+        await ctx.send(image_url)
 
 @promt.error
 async def info_error(ctx, error):
